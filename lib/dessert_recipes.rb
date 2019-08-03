3806accc1488ab11ee5c77726fd1aab7a42389b3
Brittany Javalera <brittjavalera@gmail.com>
2019-08-03 00:49:37 +0000

    added requirements to environment file

--- lib/dessert_recipes.rb
+++ lib/dessert_recipes.rb
@@ -1,6 +1,14 @@
-require "dessert_recipes/version"
-
 module DessertRecipes
-  class Error < StandardError; end
-  # Your code goes here...
+  # class Error < StandardError; end
+  # # Your code goes here...
 end
+
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative './dessert_recipes/scraper'
+require_relative './dessert_recipes/cli'
+require_relative './dessert_recipes/version'
+require_relative './dessert_recipes/recipes
