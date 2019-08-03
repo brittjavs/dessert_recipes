02278a9dd68dd9681f6fca0bba0cbc82911e30d2
Brittany Javalera <brittjavalera@gmail.com>
2019-08-03 20:31:33 +0000

    added attr_accessors, and class methods to Recipes class

--- lib/dessert_recipes/recipes.rb
+++ lib/dessert_recipes/recipes.rb
@@ -1,2 +1,19 @@
 class DessertRecipes::Recipes
+  attr_accessor :name, :url, :description, :ingredients, :instructions, :prep_time, :cook_time
+  @@all = []
+  
+  def self.initialize(name, url)
+    @name = name
+    @url = url
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
 end
--- lib/dessert_recipes/scraper.rb
+++ lib/dessert_recipes/scraper.rb
@@ -1,11 +1,13 @@
 class DessertRecipes::Scraper
+  
   def self.scrape_main_page
-    recipes = []
     doc = Nokogiri::HTML(open("https://www.loveandlemons.com/recipes/dessert-recipes"))
     doc.css(".lnl-rb-parent li").each do |recipe|
-    recipes << {:name => recipe.css(".thumbnail_text_content").text.strip,
-      :url => recipe.css("a").attribute("href").value}
-    binding.pry
+      name = recipe.css(".thumbnail_text_content").text.strip,
+      url = recipe.css("a").attribute("href").value
+      DessertRecipes::Recipes.new(name, url)
   end
   end
+  
+  
 end
