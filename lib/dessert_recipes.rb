94b536e524313bc5f02a337f15a7767a5b99129b
Brittany Javalera <brittjavalera@gmail.com>
2019-08-03 00:27:33 +0000

    first commit

---
+++ lib/dessert_recipes.rb
@@ -0,0 +1,6 @@
+require "dessert_recipes/version"
+
+module DessertRecipes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dessert_recipes/version.rb
@@ -0,0 +1,3 @@
+module DessertRecipes
+  VERSION = "0.1.0"
+end
