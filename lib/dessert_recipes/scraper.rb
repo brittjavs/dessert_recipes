3867e76dedfbc38ec54bc09dcc2b7ca33040fce3
Brittany Javalera <brittjavalera@gmail.com>
2019-08-03 01:33:38 +0000

    added url to recipes

--- lib/dessert_recipes/scraper.rb
+++ lib/dessert_recipes/scraper.rb
@@ -3,7 +3,8 @@ class DessertRecipes::Scraper
     recipes = []
     doc = Nokogiri::HTML(open("https://www.loveandlemons.com/recipes/dessert-recipes"))
     doc.css(".lnl-rb-parent li").each do |recipe|
-    recipes << {:name => recipe.css(".thumbnail_text_content").text.strip}
+    recipes << {:name => recipe.css(".thumbnail_text_content").text.strip,
+      :url => recipe.css("a").attribute("href").value}
     binding.pry
   end
   end
