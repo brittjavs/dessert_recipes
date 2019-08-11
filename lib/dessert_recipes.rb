2bab717c430cbe68c7e925d1ada6cd207923f0d5
Brittany Javalera <brittjavalera@gmail.com>
2019-08-11 03:29:32 +0000

    added colorize to cli

--- lib/dessert_recipes.rb
+++ lib/dessert_recipes.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
-
+require "colorize"
 module DessertRecipes
 require_relative './dessert_recipes/scraper'
 require_relative './dessert_recipes/cli'
--- lib/dessert_recipes/cli.rb
+++ lib/dessert_recipes/cli.rb
@@ -6,8 +6,7 @@ class DessertRecipes::CLI
   end
   
   def welcome
-  puts "Welcome! Browse through the recipes and when you are ready, enter the number for the recipe you would like to view. To exit, type 'exit'."
-  sleep 1.5
+  puts "Welcome! Browse through the recipes and when you are ready, enter the number for the recipe you would like to view. To exit, type 'exit'.".colorize(:yellow)
   end
 
   def list_recipes
@@ -24,10 +23,10 @@ def get_recipe
   
    if input.to_i.between?(1, 30)
      recipe = DessertRecipes::Recipes.all[input.to_i-1]
-     puts recipe.name
+     puts recipe.name.colorize(:yellow)
      puts ""
      DessertRecipes::Scraper.scrape_recipe_info(recipe)
-     puts recipe.description
+     puts recipe.description.colorize
      puts "\nTotal Time (Prep + Cook Time):"
      puts recipe.total_time
      puts ""
@@ -37,19 +36,19 @@ def get_recipe
      puts "\nInstructions"
      recipe.instructions.each_with_index {|step, index|
        puts "#{index + 1}. #{step}"}
-     puts "\nIf you would like to view another recipe, please enter 'list'. If you would like to exit, enter 'exit'."
+     puts "\nIf you would like to view another recipe, please enter 'list'. If you would like to exit, enter 'exit'.".colorize(:green)
      
     elsif input == "list"
-      puts "You have returned to the recipe list."
+      puts "You have returned to the recipe list.".colorize(:yellow)
       list_recipes
-      puts "\nPlease enter the number for recipe you would like to view."
+      puts "\nPlease enter the number for recipe you would like to view.".colorize(:green)
       
     elsif input == "exit"  
-      puts "Remember: You are what you eat, so eat something sweet! Goodbye"
+      puts "Remember: You are what you eat, so eat something sweet! Goodbye!".colorize(:light_blue)
       exit
       
     else
-      puts "Invalid entry. To return to the recipe list, enter 'list'. To exit, enter 'exit'."
+      puts "Invalid entry! To return to the recipe list, enter 'list'. To exit, enter 'exit'.".colorize(:red)
     end
   end
  end
